841ea99787d1fcb6f39b55905d93f06ad4ddea84
Sere27 <vetbat1126@gmail.com>
2020-10-11 18:28:24 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const APP_ID = "21bbdb7c";
+	const APP_KEY = "1ae4291cf7a9112c283d257db8eccb9b	";
+
+	const exapleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+	return (
+		<div className="App">
+			<h1></h1>
+		</div>
+	);
+};
 
 export default App;
