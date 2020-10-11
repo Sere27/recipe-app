744b9fc16a0a8b63d455c4d24bd17b4d0e7d30f8
Sere27 <vetbat1126@gmail.com>
2020-10-11 19:53:54 +0300

    useEffect added but not used

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
 	const APP_ID = "21bbdb7c";
 	const APP_KEY = "1ae4291cf7a9112c283d257db8eccb9b	";
+	const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-	const exapleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+	const [counter, setCounter] = useState(0);
+
+	useEffect(() => {
+		console.log("effect has been run");
+	});
 
 	return (
 		<div className="App">
-			<h1></h1>
+			<form className="search-form">
+				<input className="search-bar" type="text"></input>
+				<button className="search-button" type="submit">
+					{" "}
+					Search{" "}
+				</button>
+				<h1 onClick={() => setCounter(counter + 1)}> {counter}</h1>
+			</form>
 		</div>
 	);
 };
