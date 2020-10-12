2d4a0c649dc813de890c9c43cabe68213c408119
Sere27 <vetbat1126@gmail.com>
2020-10-12 13:23:55 +0300

    recipes,search and getRecipes added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
 import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
 import "./App.css";
+import { Button } from "reactstrap";
 
 const App = () => {
 	const APP_ID = "21bbdb7c";
 	const APP_KEY = "1ae4291cf7a9112c283d257db8eccb9b	";
-	const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-	const [counter, setCounter] = useState(0);
+	const [recipes, setRecipes] = useState([]);
+	const [search, setSearch] = useState("");
+	const [query, setQuery] = useState("chicken");
 
 	useEffect(() => {
-		console.log("effect has been run");
-	});
+		//sadeece counter değişince çalışcak boş olursa 1 kere
+		getRecipes();
+	}, [query]);
+
+	const getRecipes = async () => {
+		// fonksiyodna async olmalı async-await kullanacaksak
+		const response = await fetch(
+			`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`,
+		);
+		const data = await response.json();
+		setRecipes(data.hits);
+		console.log("data.hits", data.hits);
+	};
+
+	const updateSearch = (e) => {
+		setSearch(e.target.value);
+	};
+
+	const getSearch = (e) => {
+		e.preventDefault();
+		setQuery(search);
+		setSearch("");
+	};
 
 	return (
 		<div className="App">
-			<form className="search-form">
-				<input className="search-bar" type="text"></input>
-				<button className="search-button" type="submit">
-					{" "}
-					Search{" "}
-				</button>
-				<h1 onClick={() => setCounter(counter + 1)}> {counter}</h1>
+			<form onSubmit={getSearch} className="search-form">
+				<input
+					className="search-bar"
+					type="text"
+					value={search}
+					onChange={updateSearch}
+				/>
+				<Button className="search-button btn" type="submit">
+					Search
+				</Button>
 			</form>
+			{recipes.map((recipe) => (
+				<Recipe
+					key={recipe.recipe.label}
+					title={recipe.recipe.label}
+					calories={recipe.recipe.calories}
+					image={recipe.recipe.image}
+					ingredients={recipe.recipe.ingredients}
+				></Recipe>
+			))}
 		</div>
 	);
 };
